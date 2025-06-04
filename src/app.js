0f0a78c15f6bc55e9c6a13ad2aff3510a0575255
KevinViera <kyohanviera@gmail.com>
2025-06-04 18:52:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    document.querySelector("#card-number").innerHTML = generateRandomNumber();
+    document.querySelector("#top-icon","#bottom-icon").innerHTML = generateRandomIcon();
+
 };
+
+let generateRandomNumber = () => {
+    let Numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K',]
+    let IndexNumbers = Math.floor(Math.random()*Numbers.length);
+    return Numbers[IndexNumbers];
+}
+let generateRandomIcon = () => {
+    let Icon = ['♦','♥','♠','♣']
+    let IndexIcon = Math.floor(Math.random()*Icon.length);
+    return Icon[IndexIcon];
+}
